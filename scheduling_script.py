19ecf96599a6ad1f7f5dd5b1a8fb41f0504bc416
Derek Lee <derekclee323@g.ucla.edu>
2023-04-09 11:35:52 -0700

    Tutors per shift normalization

--- scheduling_script.py
+++ scheduling_script.py
@@ -21,6 +21,18 @@ from absl import flags
 
 from google.protobuf import text_format
 from ortools.sat.python import cp_model
+import pandas as pd
+
+days = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri']
+shifts = ['9-10', '10-11', '11-12', '12-1', '1-2', '2-3', '3-4', '4-5']
+SHIFTS_PER_TUTOR = 2 # TODO: Changing this will break everything for my consecutive shifts code. So... don't change it please.
+EQUALITY_WEIGHT = 10 # "Normalizes" tutors per shift
+
+# Arbitrary weights. Might require some tuning
+FIRST_CHOICE_WEIGHT = 20
+SECOND_CHOICE_WEIGHT = 10
+THIRD_CHOICE_WEIGHT = 5
+
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string('output_proto', '',
@@ -28,106 +40,11 @@ flags.DEFINE_string('output_proto', '',
 flags.DEFINE_string('params', 'max_time_in_seconds:10.0',
                     'Sat solver parameters.')
 
-def negated_bounded_span(works, start, length):
-    """Filters an isolated sub-sequence of variables assined to True.
-  Extract the span of Boolean variables [start, start + length), negate them,
-  and if there is variables to the left/right of this span, surround the span by
-  them in non negated form.
-  Args:
-    works: a list of variables to extract the span from.
-    start: the start to the span.
-    length: the length of the span.
-  Returns:
-    a list of variables which conjunction will be false if the sub-list is
-    assigned to True, and correctly bounded by variables assigned to False,
-    or by the start or end of works.
-  """
-    sequence = []
-    # Left border (start of works, or works[start - 1])
-    if start > 0:
-        sequence.append(works[start - 1])
-    for i in range(length):
-        sequence.append(works[start + i].Not())
-    # Right border (end of works or works[start + length])
-    if start + length < len(works):
-        sequence.append(works[start + length])
-    return sequence
-
-def add_soft_sequence_constraint(model, works, hard_min, soft_min, min_cost,
-                                 soft_max, hard_max, max_cost, prefix):
-    """Sequence constraint on true variables with soft and hard bounds.
-  This constraint look at every maximal contiguous sequence of variables
-  assigned to true. If forbids sequence of length < hard_min or > hard_max.
-  Then it creates penalty terms if the length is < soft_min or > soft_max.
-  Args:
-    model: the sequence constraint is built on this model.
-    works: a list of Boolean variables.
-    hard_min: any sequence of true variables must have a length of at least
-      hard_min.
-    soft_min: any sequence should have a length of at least soft_min, or a
-      linear penalty on the delta will be added to the objective.
-    min_cost: the coefficient of the linear penalty if the length is less than
-      soft_min.
-    soft_max: any sequence should have a length of at most soft_max, or a linear
-      penalty on the delta will be added to the objective.
-    hard_max: any sequence of true variables must have a length of at most
-      hard_max.
-    max_cost: the coefficient of the linear penalty if the length is more than
-      soft_max.
-    prefix: a base name for penalty literals.
-  Returns:
-    a tuple (variables_list, coefficient_list) containing the different
-    penalties created by the sequence constraint.
-  """
-    cost_literals = []
-    cost_coefficients = []
-
-    # Forbid sequences that are too short.
-    for length in range(1, hard_min):
-        for start in range(len(works) - length + 1):
-            model.AddBoolOr(negated_bounded_span(works, start, length))
-
-    # Penalize sequences that are below the soft limit.
-    if min_cost > 0:
-        for length in range(hard_min, soft_min):
-            for start in range(len(works) - length + 1):
-                span = negated_bounded_span(works, start, length)
-                name = ': under_span(start=%i, length=%i)' % (start, length)
-                lit = model.NewBoolVar(prefix + name)
-                span.append(lit)
-                model.AddBoolOr(span)
-                cost_literals.append(lit)
-                # We filter exactly the sequence with a short length.
-                # The penalty is proportional to the delta with soft_min.
-                cost_coefficients.append(min_cost * (soft_min - length))
-
-    # Penalize sequences that are above the soft limit.
-    if max_cost > 0:
-        for length in range(soft_max + 1, hard_max + 1):
-            for start in range(len(works) - length + 1):
-                span = negated_bounded_span(works, start, length)
-                name = ': over_span(start=%i, length=%i)' % (start, length)
-                lit = model.NewBoolVar(prefix + name)
-                span.append(lit)
-                model.AddBoolOr(span)
-                cost_literals.append(lit)
-                # Cost paid is max_cost * excess length.
-                cost_coefficients.append(max_cost * (length - soft_max))
-
-    # Just forbid any sequence of true variables with length hard_max + 1
-    for start in range(len(works) - hard_max):
-        model.AddBoolOr(
-            [works[i].Not() for i in range(start, start + hard_max + 1)])
-    return cost_literals, cost_coefficients
-
-
 def solve_shift_scheduling(params, output_proto):
     """Solves the shift scheduling problem."""
     # Data
     num_employees = 100
-    shifts = ['9-10', '10-11', '11-12', '12-1', '1-2', '2-3', '3-4', '4-5']
-
-    num_days = 5
+    num_days = len(days)
     num_shifts = len(shifts)
 
     model = cp_model.CpModel()
@@ -139,7 +56,6 @@ def solve_shift_scheduling(params, output_proto):
             for d in range(num_days):
                 work[e, s, d] = model.NewBoolVar('work%i_%i_%i' % (e, s, d))
 
-
     #### Hard Constraints
 
     # Two shifts per tutor
@@ -149,7 +65,7 @@ def solve_shift_scheduling(params, output_proto):
             for d in range(num_days):
                 total_shifts.append(work[e, s, d])
                 
-        model.Add(sum(total_shifts) == 2)
+        model.Add(sum(total_shifts) == SHIFTS_PER_TUTOR)
     
     # At least 1 tutor per shift (Maybe this should be a soft constraint, we'll see)
     for d in range(num_days):
@@ -172,12 +88,33 @@ def solve_shift_scheduling(params, output_proto):
     ##### Soft Constraints
 
     # Tutor Preferences
+    tutor_preferences = []
+    tutor_preference_weights = []
 
-    # Normalize number of people per shift
+    #test = True
+    #if test:
+    #    tutor_preferences.append(work[1, 6, 4])
+    #    tutor_preference_weights.append(10)
 
 
-    # Normalize class variety (probably not that important)
-
+    # Normalize number of people per shift (assumes equal workload per shift)
+    # TODO: modify to be based on shift traffic
+    min_demand = num_employees * SHIFTS_PER_TUTOR // (num_shifts * num_days)
+    worked = {}
+    delta = model.NewIntVar(0, num_employees, "delta")
+    for s in range(num_shifts):
+        for d in range(5):
+            works = [work[e, s, d] for e in range(num_employees)]
+            worked[(s, d)] = model.NewIntVar(min_demand, num_employees, '')
+            model.Add(worked[s, d] == sum(works))
+            model.Add(worked[s, d] >= min_demand - delta)
+            model.Add(worked[s, d] <= min_demand - delta)
+    
+    # TODO: Normalize class variety (probably not that important)
+    
+    model.Maximize(sum(tutor_preferences[i] * tutor_preference_weights[i] for i in range(len(tutor_preferences)))
+                    - delta * EQUALITY_WEIGHT
+                    )
 
     if output_proto:
         print('Writing proto to %s' % output_proto)
@@ -193,7 +130,7 @@ def solve_shift_scheduling(params, output_proto):
 
     # Print solution.
     if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-        days = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri']
+        
         print()
         for d in range(num_days):
             print(days[d] + '------------------------------------------')
